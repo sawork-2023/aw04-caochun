16472503f54b6c0156c8b1d7c2abd9d145de8155
Chun <caochun@gmail.com>
2023-03-28 09:09:50 +0800

    update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO TBL_PRODUCTS (prd_category, prd_name, prd_price)
-SELECT * FROM CSVREAD('/home/chun/develop/aw04-caochun/target/classes/data.csv', NULL, 'charset=UTF-8');
+SELECT * FROM CSVREAD('classpath:/data.csv', NULL, 'charset=UTF-8');
